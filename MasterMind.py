630b58318583d2bd06f29adbf0f4d86eb61ec9d9
Daan Kleijn <daan.kleijn@student.hu.nl>
2022-02-15 11:20:41 +0100

    Found bug and made prints to see what is wrong

--- MasterMind.py
+++ MasterMind.py
@@ -3,25 +3,26 @@ import random
 
 def makePassWord():
     colors = "blue", "yellow", "red", "purple", "orange", "green"
-    global passWord
+
     passWord = []
     for i in range (4):
         passWordInput = random.choice(colors)
         passWord.append(passWordInput)
     print(passWord)
+    return passWord
 
 
 def guess():
-    global answer
     answer = input("Guess 4 colors: ")
-
+    return answer
 
 
 def game():
-    global passWord
-    global answer
-    makePassWord()
-    guess()
+    passWord = makePassWord()
+    answer = guess()
+    print(passWord)
+    print(answer)
+
     if answer == passWord:
         return f"Congratulations you've guessed the code in one try!"
     else:
@@ -29,42 +30,41 @@ def game():
         while (answer != passWord):
             tries += 1
             correct = 0
-            answer = str(answer)
-            passWord= str(passWord)
             correctlst = []
             rightPlace = "w"
             rightButWrong = "z"
 
             for i in range(4):
                 correctlst.append("x")
+                print(answer[i])
                 if (answer[i] == passWord[i]):
                     correct += 1
                     correctlst.pop()
                     correctlst.append(rightPlace)
-                    print(correctlst)
 
                 else:
                     continue
 
-            if (correct < 4) and (correct != 0):
-                print("You got ", correct, " color(s) correct!")
-                print("Also these colors in your input were correct.")
+            for i in range(10):
+                if (correct < 4) and (correct != 0):
+                    print("You got ", correct, " color(s) correct!")
+                    print("Also these colors in your input were correct.")
 
-                for k in correctlst:
-                    print(k, end=' ')
-                print('\n')
-                answer = guess()
+                    for k in correctlst:
+                        print(k, end=' ')
+                    print('\n')
+                    answer = guess()
 
-            elif (correct == 0):
-                print("None of the colors in your input match.")
-                answer = guess()
+                elif (correct == 0):
+                    print("None of the colors in your input match.")
+                    answer = guess()
 
-            elif tries == 10:
-                return f"No turns left"
+                elif tries == 10:
+                    return f"No turns left"
 
-        if answer == passWord:
-            print("You've become a Mastermind!")
-            print("It took you only", tries, "tries.")
+            if answer == passWord:
+                print("You've become a Mastermind!")
+                print("It took you only", tries, "tries.")
 
 
 print(game())
