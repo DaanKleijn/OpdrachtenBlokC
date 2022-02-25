b87eb54f4bd0067f9ec73492f3c4229b2328c65b
Daan Kleijn <daan.kleijn@student.hu.nl>
2022-02-25 10:46:07 +0100

    Guess game working

--- MasterMind.py
+++ MasterMind.py
@@ -1,9 +1,24 @@
 import random
 
 
+def whatUser():
+    userInput = input("Would you like to play mastermind against a computer? y or n ")
+    if "y" in userInput:
+        userInput = input("Would you like to play as guesser or make the code? guess or code ")
+        if "guess" in userInput:
+            game()
+        else:
+            userInput = ("Would you like to use the simple guess or a bit more advanced? simple or advanced ")
+            if "simple" in userInput:
+                computerSimpleGuess()
+            else:
+                computerRandomGuess()
+    else:
+        return f"Allrighty then."
+
+
 def makepassword():
     colors = "blue", "yellow", "red", "purple", "orange", "green"
-
     passWord = []
     for i in range(4):
         passWordInput = random.choice(colors)
@@ -22,56 +37,64 @@ def guess():
     return answerlst
 
 
-def whatUser():
-    userInput = input("Would you like to play mastermind against a computer? y or n ")
-    if "y" in userInput:
-        userInput = input("Would you like to play as guesser or make the code? guess or code ")
-        if "guess" in userInput:
-            game()
-        else:
-            computerGuess()
-    else:
-        return f"Allrighty then."
-
-
 def game():
     passWord = makepassword()
     answer = guess()
-    score = [0, 0]
+    while answer != passWord:
+        score = [0, 0]
+        restAnswer = []
+        restPassword = []
 
-    if answer == passWord:
-        return f"Congratulations you've guessed the code in one try!!"
-    else:
-        tries = 0
-        almostCorrect = 0
-        while answer != passWord:
-            tries += 1
+        if answer == passWord:
+            return f"Congratulations you've guessed the code in one try!!"
+        else:
+            tries = 0
+            almostCorrect = 0
             correct = 0
 
-            for i in range(4):
+            for i in range(0, 4):
                 if answer[i] == passWord[i]:
                     correct += 1
                     score[0] = correct
+                else:
+                    restAnswer.append(answer[i])
+                    restPassword.append(passWord[i])
 
-                elif answer[i] != passWord[i]:
+            for i in range(len(restAnswer)):
+                if restAnswer[i] in restPassword:
                     almostCorrect += 1
+                    score[-1] = almostCorrect
+                    restPassword.remove(restAnswer[i])
 
-            if correct < 4 and correct != 4:
-                score[-1] = almostCorrect
+            if correct < 4:
                 print("\n")
                 print(score)
                 answer = guess()
-                score = [0, 0]
-                almostCorrect = 0
+                print(answer)
 
-            elif correct == 0:
-                print("None of the numbers in your input match.")
-                answer = guess()
+        if correct == 4:
+            print("You've cracked the code! It took you only", tries, "tries.")
 
-        if answer == passWord:
-            return "You've cracked the code! It took you only", tries, "tries."
 
-def computerGuess():
+def computerSimpleGuess():
+    colors = "blue", "yellow", "red", "purple", "orange", "green"
+    passwordlst = []
+    computerAnswer = ["blue", "blue", "blue", "blue"]
+    nums = 1
+    tries = 0
+    print(colors)
+    while len(passwordlst) != 4:
+        passWord = input("What will be color " + str(nums) + "? ")
+        if passWord in colors:
+            passwordlst.append(passWord)
+            nums += 1
+
+    while computerAnswer != passwordlst:
+        computerAnswer[-1] += 1
+        print(computerAnswer)
+
+
+def computerRandomGuess():
     colors = "blue", "yellow", "red", "purple", "orange", "green"
     passwordlst = []
     computerAnswer = []
